b1049fdc9d03d0dea92d9649876acfe95f1ac07d
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-30 11:38:31 -0700

    init server files

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+const logger = (req, res, next) => {
+  console.log(`Receiving request to ${req.url} with method ${req.method}`);
+  next();
+};
+
+app.use('/', logger);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
+
+module.exports = {
+  app,
+  server,
+};
